1a8be4d15af9a12b097323bda4a4d97c7ba2e787
virgilsisoe <virgilsisoe@gmail.com>
2022-03-17 15:50:39 -0400

    put the cursor in position after document was modified

--- src/insert_import.ts
+++ src/insert_import.ts
@@ -11,33 +11,40 @@ export function insertImport(args: string) {
 
     const document = editor.document;
     let docText = document.getText();
+    let lineWasAdded = false;
 
     for (let i = 0; i < document.lineCount; i++) {
         const line = document.lineAt(i).text;
 
         if (line.match(parent)) {
-            console.log("🚀 ~ line", line);
-
             let replaceRegex: RegExp;
             let replaceString: string;
 
             if (/\(/.test(line)) {
                 replaceRegex = RegExp(`\(?<=${parent}\.+\\(\)`);
                 replaceString = `\n    ${module},`;
+                lineWasAdded = true;
             } else {
                 replaceRegex = RegExp(`\(?<=${parent}\\simport\\s\)`);
-                replaceString = `${module},`;
+                replaceString = `${module}, `;
             }
 
             docText = docText.replace(replaceRegex, replaceString);
         }
     }
 
+    const position = editor.selection.active;
+
     editor.edit((editBuilder) => {
         const firstLine = document.lineAt(0);
         const lastLine = document.lineAt(document.lineCount - 1);
         const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-
         editBuilder.replace(textRange, docText);
     });
+
+    // need to se back the cursor after modifying the document
+    const line = lineWasAdded ? position.line + 1 : position.line;
+    const newPosition = position.with(line, position.character);
+    const newSelection = new vscode.Selection(newPosition, newPosition);
+    editor.selection = newSelection;
 }
